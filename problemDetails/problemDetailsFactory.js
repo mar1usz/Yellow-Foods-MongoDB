818b6e5bf41db5154190b026cf363ff8bf903737
Mariusz <mariopl@poczta.onet.eu>
2021-02-03 22:07:25 +0100

    add type checks to problem details factory

--- problemDetails/problemDetailsFactory.js
+++ problemDetails/problemDetailsFactory.js
@@ -6,20 +6,15 @@ exports.problemDetailsFactory = {
     title = null,
     type = null,
     extensions = null } = {}) {
-    // apply defaults
-    status ??= 500;
-    title ??= 'An error occured while processing your request.';
-    type ??= 'https://tools.ietf.org/html/rfc7231#section-6.6.1';
-
-    // create new problem details object
+    // create new problem details
     let problemDetails = {};
 
     // assign properties
-    if (type) problemDetails['type'] = type;
-    if (title) problemDetails['title'] = title;
-    if (status) problemDetails['status'] = status;
-    if (detail) problemDetails['detail'] = detail;
-    if (instance) problemDetails['instance'] = instance;
+    if (type && typeof (type) === 'string') problemDetails['type'] = type;
+    if (title && typeof (title) === 'string') problemDetails['title'] = title;
+    if (status && typeof (status) === 'number') problemDetails['status'] = status;
+    if (detail && typeof (detail) === 'string') problemDetails['detail'] = detail;
+    if (instance && typeof (instance) === 'string') problemDetails['instance'] = instance;
 
     // assign extension members
     if (extensions && extensions instanceof Object) {
@@ -27,6 +22,11 @@ exports.problemDetailsFactory = {
         problemDetails[key] = value;
       }
     }
+
+    // apply defaults
+    problemDetails['type'] ??= 'https://tools.ietf.org/html/rfc7231#section-6.6.1';
+    problemDetails['title'] ??= 'An error occured while processing your request.';
+    problemDetails['status'] ??= 500;
 
     return problemDetails;
   }

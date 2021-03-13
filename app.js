dbb46576e5574e0abd29f15f28145a0a4907740f
Mariusz <mariopl@poczta.onet.eu>
2021-03-13 14:52:31 +0100

    remove line breaks between imports

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
 const foodsController = require('./controllers/foodsController');
 const {
   createNotFound
--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Food = require('../models/food');
-
 const {
   createNotFound,
   createProblem,

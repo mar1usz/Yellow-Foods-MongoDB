6a3fcea5388254372249a7996aeebbc929219dbd
mar1usz <mariopl@poczta.onet.eu>
2021-03-18 19:42:03 +0100

    use dot syntax

--- problemDetails/problemDetailsFactory.js
+++ problemDetails/problemDetailsFactory.js
@@ -9,12 +9,12 @@ exports.problemDetailsFactory = {
   } = {}) {
     const problemDetails = {};
 
-    if (type && typeof type === 'string') problemDetails['type'] = type;
-    if (title && typeof title === 'string') problemDetails['title'] = title;
-    if (status && typeof status === 'number') problemDetails['status'] = status;
-    if (detail && typeof detail === 'string') problemDetails['detail'] = detail;
+    if (type && typeof type === 'string') problemDetails.type = type;
+    if (title && typeof title === 'string') problemDetails.title = title;
+    if (status && typeof status === 'number') problemDetails.status = status;
+    if (detail && typeof detail === 'string') problemDetails.detail = detail;
     if (instance && typeof instance === 'string')
-      problemDetails['instance'] = instance;
+      problemDetails.instance = instance;
 
     if (extensions && extensions instanceof Object) {
       for (const [key, value] of Object.entries(extensions)) {
@@ -24,12 +24,10 @@ exports.problemDetailsFactory = {
 
     // apply defaults
     if ('type' in problemDetails === false)
-      problemDetails['type'] =
-        'https://tools.ietf.org/html/rfc7231#section-6.6.1';
+      problemDetails.type = 'https://tools.ietf.org/html/rfc7231#section-6.6.1';
     if ('title' in problemDetails === false)
-      problemDetails['title'] =
-        'An error occured while processing your request.';
-    if ('status' in problemDetails === false) problemDetails['status'] = 500;
+      problemDetails.title = 'An error occured while processing your request.';
+    if ('status' in problemDetails === false) problemDetails.status = 500;
 
     return problemDetails;
   }

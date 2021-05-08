87cb541e6d60b5c4cc353a979b7a0631969c8ab2
Mariusz <mariopl@poczta.onet.eu>
2021-05-08 22:37:46 +0200

    fix nutrientEntry unique index

--- models/nutrient-entry.js
+++ models/nutrient-entry.js
@@ -9,6 +9,6 @@ const NutrientEntrySchema = new Schema({
   amount: { type: Types.Decimal128, required: true }
 });
 
-NutrientEntrySchema.index({ food: 1, nutrient: 1 }, { unique: true });
+NutrientEntrySchema.index({ food_id: 1, nutrient_id: 1 }, { unique: true });
 
 module.exports = mongoose.model('NutrientEntry', NutrientEntrySchema);

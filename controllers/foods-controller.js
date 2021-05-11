9431317a786355bf3261ba71dd79f1e237c14ebe
Mariusz <mariopl@poczta.onet.eu>
2021-05-11 08:10:57 +0200

    refactor foddsRouter into router and controller

---
+++ controllers/foods-controller.js
@@ -0,0 +1,98 @@
+const Food = require('../models/food');
+const {
+  createNotFound,
+  createValidationProblem
+} = require('../problem-details/problem-details-convenience-methods');
+const { body, param, validationResult } = require('express-validator');
+
+exports.getFoods = async (req, res, next) => {
+  const foods = await Food.find();
+  res.status(200).json(foods.map((f) => toJson(f)));
+};
+
+exports.getFood = async (req, res, next) => {
+  const food = await Food.findById(req.params._id);
+
+  if (food === null) {
+    const notFound = createNotFound();
+    res.status(notFound.status).problemJson(notFound);
+    return;
+  }
+
+  res.status(200).json(toJson(food));
+};
+
+exports.postFood = [
+  body('name').isString(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationProblem = createValidationProblem({
+        errors: errors.array()
+      });
+      res.status(validationProblem.status).problemJson(validationProblem);
+      return;
+    }
+
+    const food = new Food({
+      name: req.body.name
+    });
+    const savedFood = await food.save();
+
+    if (savedFood === null) {
+      const notFound = createNotFound();
+      res.status(notFound.status).problemJson(notFound);
+      return;
+    }
+
+    res.status(201).json(toJson(savedFood));
+  }
+];
+
+exports.putFood = [
+  param('_id')
+    .isMongoId()
+    .custom((_id, { req }) => _id === req.body._id),
+  body('_id').isMongoId(),
+  body('name').isString(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationProblem = createValidationProblem({
+        errors: errors.array()
+      });
+      res.status(validationProblem.status).problemJson(validationProblem);
+      return;
+    }
+
+    const food = new Food({
+      _id: req.body._id,
+      name: req.body.name
+    });
+    const updatedFood = await Food.findByIdAndUpdate(req.params._id, food);
+
+    if (updatedFood === null) {
+      const notFound = createNotFound();
+      res.status(notFound.status).problemJson(notFound);
+      return;
+    }
+
+    res.status(204).json();
+  }
+];
+
+exports.deleteFood = async (req, res, next) => {
+  const removedFood = await Food.findByIdAndRemove(req.params._id);
+
+  if (removedFood === null) {
+    const notFound = createNotFound();
+    res.status(notFound.status).problemJson(notFound);
+    return;
+  }
+
+  res.status(200).json(toJson(removedFood));
+};
+
+function toJson(food) {
+  return { _id: food._id, name: food.name };
+}
--- routes/foods-router.js
+++ routes/foods-router.js
@@ -1,101 +1,19 @@
 const express = require('express');
-const Food = require('../models/food');
 const {
-  createNotFound,
-  createValidationProblem
-} = require('../problem-details/problem-details-convenience-methods');
-const { body, param, validationResult } = require('express-validator');
+  getFoods,
+  getFood,
+  postFood,
+  putFood,
+  deleteFood
+} = require('../controllers/foods-controller');
 
 const router = express.Router();
 
-router.get('/foods', async (req, res, next) => {
-  const foods = await Food.find();
-  res.status(200).json(foods.map((f) => toJson(f)));
-});
-
-router.get('/foods/:_id', async (req, res, next) => {
-  const food = await Food.findById(req.params._id);
-
-  if (food === null) {
-    const notFound = createNotFound();
-    res.status(notFound.status).problemJson(notFound);
-    return;
-  }
-
-  res.status(200).json(toJson(food));
-});
-
-router.post('/foods', body('name').isString(), async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const validationProblem = createValidationProblem({
-      errors: errors.array()
-    });
-    res.status(validationProblem.status).problemJson(validationProblem);
-    return;
-  }
-
-  const food = new Food({
-    name: req.body.name
-  });
-  const savedFood = await food.save();
-
-  if (savedFood === null) {
-    const notFound = createNotFound();
-    res.status(notFound.status).problemJson(notFound);
-    return;
-  }
-
-  res.status(201).json(toJson(savedFood));
-});
-
-router.put(
-  '/foods/:_id',
-  param('_id')
-    .isMongoId()
-    .custom((_id, { req }) => _id === req.body._id),
-  body('_id').isMongoId(),
-  body('name').isString(),
-  async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const validationProblem = createValidationProblem({
-        errors: errors.array()
-      });
-      res.status(validationProblem.status).problemJson(validationProblem);
-      return;
-    }
-
-    const food = new Food({
-      _id: req.body._id,
-      name: req.body.name
-    });
-    const updatedFood = await Food.findByIdAndUpdate(req.params._id, food);
-
-    if (updatedFood === null) {
-      const notFound = createNotFound();
-      res.status(notFound.status).problemJson(notFound);
-      return;
-    }
-
-    res.status(204).json();
-  }
-);
-
-router.delete('/foods/:_id', async (req, res, next) => {
-  const removedFood = await Food.findByIdAndRemove(req.params._id);
-
-  if (removedFood === null) {
-    const notFound = createNotFound();
-    res.status(notFound.status).problemJson(notFound);
-    return;
-  }
-
-  res.status(200).json(toJson(removedFood));
-});
-
-function toJson(food) {
-  return { _id: food._id, name: food.name };
-}
+const foods_route = '/foods';
+router.get(foods_route, getFoods);
+router.get(foods_route + '/:_id', getFood);
+router.post(foods_route, postFood);
+router.put(foods_route + '/:_id', putFood);
+router.delete(foods_route + '/:_id', deleteFood);
 
 module.exports = router;

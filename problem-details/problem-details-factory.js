a4e0954bfddd3dd0137299638c65bf8deb48a466
mar1usz <mariopl@poczta.onet.eu>
2021-05-18 18:27:46 +0200

    Revert "replace Object.assign with spread operator"
    This reverts commit d3f45bc9aa80546a8c5eab5cdfa7a677468ef925.

--- problem-details/problem-details-factory.js
+++ problem-details/problem-details-factory.js
@@ -5,27 +5,26 @@ exports.problemDetailsFactory = {
     status = null,
     title = null,
     type = null,
-    extensions = null,
+    extensions = null
   } = {}) {
-    let problemDetails = {};
+    const problemDetails = {};
 
-    if (type && typeof type === "string") problemDetails.type = type;
-    if (title && typeof title === "string") problemDetails.title = title;
-    if (status && typeof status === "number") problemDetails.status = status;
-    if (detail && typeof detail === "string") problemDetails.detail = detail;
-    if (instance && typeof instance === "string")
+    if (type && typeof type === 'string') problemDetails.type = type;
+    if (title && typeof title === 'string') problemDetails.title = title;
+    if (status && typeof status === 'number') problemDetails.status = status;
+    if (detail && typeof detail === 'string') problemDetails.detail = detail;
+    if (instance && typeof instance === 'string')
       problemDetails.instance = instance;
 
-    if (extensions && extensions instanceof Object) {
-      problemDetails = { ...problemDetails, ...extensions };
-    }
+    if (extensions && extensions instanceof Object)
+      Object.assign(problemDetails, extensions);
 
-    if ("type" in problemDetails === false)
-      problemDetails.type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
-    if ("title" in problemDetails === false)
-      problemDetails.title = "An error occured while processing your request.";
-    if ("status" in problemDetails === false) problemDetails.status = 500;
+    if ('type' in problemDetails === false)
+      problemDetails.type = 'https://tools.ietf.org/html/rfc7231#section-6.6.1';
+    if ('title' in problemDetails === false)
+      problemDetails.title = 'An error occured while processing your request.';
+    if ('status' in problemDetails === false) problemDetails.status = 500;
 
     return problemDetails;
-  },
+  }
 };
